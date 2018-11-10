d0c03af8334d57f31cca10767ad9fd969c201778
Jeff Peterson <jeff@yak.sh>
2018-11-10 04:01:05 -0800

    Support widgets with no ports

--- src/js/ElmApp.ts
+++ src/js/ElmApp.ts
@@ -1,33 +1,42 @@
 import QueuedResource from "./QueuedResource"
 
-export interface ElmApp {
-  ports: {
-    output: {
-      subscribe(fn: Function): void
-      unsubscribe(fn: Function): void
-    }
-    input: {
-      send(msg: any): void
-    }
+export interface Ports {
+  output: {
+    subscribe(fn: Function): void
+    unsubscribe(fn: Function): void
   }
+  input: {
+    send(msg: any): void
+  }
+}
+
+export interface ElmApp {
+  ports?: Ports
 }
 
 export default class QueuedElmApp extends QueuedResource<any, any> {
   app: ElmApp
+  ports?: Ports
 
   constructor(app: ElmApp, name?: string) {
     super(name || "ElmApp")
+
+    const { ports } = app
+
     this.app = app
+    this.ports = ports
 
-    app.ports.output.subscribe(this.receiveQ.push)
+    if (ports) {
+      ports.output.subscribe(this.receiveQ.push)
 
-    this.sendQ.subscribe(msg => {
-      app.ports.input.send(msg)
-    })
+      this.sendQ.subscribe(msg => {
+        ports.input.send(msg)
+      })
+    }
   }
 
   close() {
     this.sendQ.unsubscribe()
-    this.app.ports.output.unsubscribe(this.receiveQ.push)
+    this.ports && this.ports.output.unsubscribe(this.receiveQ.push)
   }
 }
