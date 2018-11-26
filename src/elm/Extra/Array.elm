ca1875da8b52d3cc29909450bdd4d844dab1c2bb
Jeff Peterson <jeff@yak.sh>
2018-11-26 15:08:27 -0800

    Add Extra.Array

---
+++ src/elm/Extra/Array.elm
@@ -0,0 +1,20 @@
+module Extra.Array exposing (remove, update)
+
+import Array exposing (..)
+
+
+update : Int -> (a -> a) -> Array a -> Array a
+update i fn arr =
+    case get i arr of
+        Nothing ->
+            arr
+
+        Just a ->
+            set i (fn a) arr
+
+
+remove : Int -> Array a -> Array a
+remove n arr =
+    append
+        (slice 0 n arr)
+        (slice (n + 1) -1 arr)
