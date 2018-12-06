80367253d416ce3096675e45186c6f72f37902de
Jeff Peterson <jeff@yak.sh>
2018-12-06 07:47:19 -0800

    Add Menus

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -2,7 +2,13 @@ process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "1"
 
 console.log("App starting...")
 
-import { app, shell, BrowserWindow } from "electron"
+import {
+  app,
+  shell,
+  Menu,
+  BrowserWindow,
+  MenuItemConstructorOptions,
+} from "electron"
 
 app.on("ready", createWindow)
 
@@ -36,6 +42,71 @@ function createWindow(): BrowserWindow {
     e.preventDefault()
     shell.openExternal(url)
   })
+
+  const template: MenuItemConstructorOptions[] = [
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "pasteandmatchstyle" },
+        { role: "delete" },
+        { role: "selectall" },
+      ],
+    },
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forcereload" },
+        { role: "toggledevtools" },
+        { type: "separator" },
+        { role: "resetzoom" },
+        { role: "zoomin" },
+        { role: "zoomout" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
+    },
+    {
+      role: "window",
+      submenu: [{ role: "minimize" }, { role: "close" }],
+    },
+    {
+      role: "help",
+      submenu: [
+        {
+          label: "Realm on Github",
+          click() {
+            shell.openExternal("https://github.com/inkandswitch/realm")
+          },
+        },
+      ],
+    },
+  ]
+
+  if (process.platform === "darwin") {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        { role: "about" },
+        { type: "separator" },
+        { role: "services", submenu: [] },
+        { type: "separator" },
+        { role: "hide" },
+        { role: "hideothers" },
+        { role: "unhide" },
+        { type: "separator" },
+        { role: "quit" },
+      ],
+    })
+  }
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
   return win
 }
