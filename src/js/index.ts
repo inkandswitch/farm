f4befba401a228a8cd0a4777ee25c7d5970c3800
Orion Henry <orion.henry@gmail.com>
2018-12-20 12:35:13 -0500

    add hyperfile handler

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,13 +2,26 @@ process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "1"
 
 import App from "./App"
 import Debug from "debug"
-import { ipcRenderer } from "electron"
+import URL from "url"
+import { ipcRenderer, remote } from "electron"
+
+const  { protocol } = remote
 
 const app = new App()
 
 Object.assign(self, {
   Debug,
   app,
+})
+
+protocol.registerBufferProtocol('hyperfile', (request, callback) => {
+  const id = request.url.slice(12)
+  console.log(`HYPERFILE-RENDER6='${id}'`)
+  app.repo.front.readFile(id,(_data, mimeType) => {
+    console.log(`HYPERFILE-RENDERb='${_data.length} bytes'`)
+    const data = Buffer.from(_data)
+    callback({ mimeType, data })
+  })
 })
 
 ipcRenderer.on("open-url", (_event: any, url: string) => {
