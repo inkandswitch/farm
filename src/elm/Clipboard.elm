4d09020a4bbd5167499b2f6f48d5c713a38d882c
Jeff Peterson <jeff@yak.sh>
2019-01-30 13:17:38 -0800

    Add doc comments to Clipboard module. might be useless

--- src/elm/Clipboard.elm
+++ src/elm/Clipboard.elm
@@ -1,8 +1,20 @@
 module Clipboard exposing (copy)
 
+{-| This module allows the copying of a <String> to the user's
+clipboard.
+
+
+# Writing to clipboard
+
+@docs copy
+
+-}
+
 import Gizmo
 
 
+{-| Copy the given String to the user's clipboard.
+-}
 copy : String -> Cmd msg
 copy str =
     Gizmo.command ( "Copy", str )
